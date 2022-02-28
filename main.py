9836226eb9fb46c566b8b5760104e7e67c32cf5b
GodSaveTheCsar <danil.kozinski@ya.ru>
2022-02-28 19:55:24 +0300

    sposobin...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+
 from flask import Flask, render_template
-from flask.ext.heroku import Heroku
 from data import db_session
 from data.termins import Term
 
@@ -7,11 +8,11 @@ app = Flask(__name__)
 db_session.global_init('db/termins.db')
 db_sess = db_session.create_session()
 termins = db_sess.query(Term).all()
-heroku = Heroku()
-
 
 @app.route('/')
 def a():
     return render_template('main_page.html', termins=termins)
 
-heroku.init_app(app)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
