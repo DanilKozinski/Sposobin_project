19a1686368f33f9fec6346c5acab11af4926077e
GodSaveTheCsar <danil.kozinski@ya.ru>
2022-02-28 19:01:41 +0300

    sposobin...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask.ext.heroku import Heroku
 from data import db_session
 from data.termins import Term
 
@@ -6,13 +7,11 @@ app = Flask(__name__)
 db_session.global_init('db/termins.db')
 db_sess = db_session.create_session()
 termins = db_sess.query(Term).all()
-
+heroku = Heroku()
 
 
 @app.route('/')
 def a():
     return render_template('main_page.html', termins=termins)
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+heroku.init_app(app)
