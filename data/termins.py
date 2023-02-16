ee9d86f34cf31007e7a02f95170944952e1b785c
Danil <danil.kozisnki@ya.ru>
2023-02-16 09:53:43 +0300

    sposobin...

--- data/termins.py
+++ data/termins.py
@@ -3,11 +3,27 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class Term(SqlAlchemyBase):
-    __tablename__ = 'terms'
+class Terms_Sposobin(SqlAlchemyBase):
+    __tablename__ = 'terms_sposobin'
     id = sqlalchemy.Column(sqlalchemy.Integer,primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     definition = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     pictures_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    music_id = sqlalchemy.Column(sqlalchemy.Integer, index=True, unique=True, nullable=True)
+    paragraph = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+
+class Terms_Vahromeev(SqlAlchemyBase):
+    __tablename__ = 'terms_vahromeev'
+    id = sqlalchemy.Column(sqlalchemy.Integer,primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    definition = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    pictures_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    paragraph = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+
+
+class Terms_Alexeev(SqlAlchemyBase):
+    __tablename__ = 'terms_alexeev'
+    id = sqlalchemy.Column(sqlalchemy.Integer,primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    definition = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    pictures_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     paragraph = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
--- main.py
+++ main.py
@@ -1,18 +1,57 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import db_session
-from data.termins import Term
+from data.termins import Terms_Vahromeev, Terms_Sposobin, Terms_Alexeev
 
 app = Flask(__name__)
 db_session.global_init('db/termins.db')
 db_sess = db_session.create_session()
-termins = db_sess.query(Term).all()
+termins_sposobin = db_sess.query(Terms_Sposobin).all()
+termins_vahromeev = db_sess.query(Terms_Vahromeev).all()
+termins_alexeev = db_sess.query(Terms_Alexeev).all()
+union = [termins_sposobin, termins_vahromeev]
 
-@app.route('/')
-def a():
-    return render_template('main_page.html', termins=termins)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        search = request.values.get('search_all', default='')
+        termins_sposobin_searched = db_sess.query(Terms_Sposobin).filter(Terms_Sposobin.name.like(f'%{search}%'))
+        termins_vahromeev_searched = db_sess.query(Terms_Vahromeev).filter(Terms_Vahromeev.name.like(f'%{search}%'))
+        termins_alexeev_searched = db_sess.query(Terms_Alexeev).filter(Terms_Alexeev.name.like(f'%{search}%'))
+        union_searched = [termins_sposobin_searched, termins_vahromeev_searched, termins_alexeev_searched]
+        for e in union_searched:
+            for i in e:
+                print(i.name, i.id)
+        print(search)
+    return render_template('main_page.html', union=union_searched)
+
+
+@app.route('/sposobin', methods=['GET', 'POST'])
+def sposobin():
+    search = request.values.get('search_s', default='')
+    termins_sposobin_searched = db_sess.query(Terms_Sposobin).filter(Terms_Sposobin.name.like(f'%{search}%'))
+    for e in termins_sposobin_searched:
+        print(e.name)
+    return render_template('sposobin.html', termins_s=termins_sposobin_searched)
+
+
+@app.route('/vahromeev', methods=['GET', 'POST'])
+def vahromeev():
+    search = request.values.get('search_v', default='')
+    termins_vahromeev_searched = db_sess.query(Terms_Vahromeev).filter(Terms_Vahromeev.name.like(f'%{search}%'))
+    return render_template('vahromeev.html', termins_v=termins_vahromeev_searched)
+
+
+@app.route('/alexeev', methods=['GET', 'POST'])
+def alexeev():
+    search = request.values.get('search_a', default='')
+    termins_alexeev_searched = db_sess.query(Terms_Alexeev).filter(Terms_Alexeev.name.like(f'%{search}%'))
+    return render_template('alexeev.html', termins_a=termins_alexeev_searched)
+
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='192.168.0.72', port=port, debug=True)
